3bf62d4caf244b249376df66a3c1b44e2ae78ec0
nwelchr <nwelchr@gmail.com>
2018-05-18 13:47:24 -0700

    Adjust frankie's jump, make 4th level

--- javascripts/frankie.js
+++ javascripts/frankie.js
@@ -5,7 +5,7 @@ class Frankie extends Player {
 constructor(pos, ch, speed, size, xSpeed, jumpSpeed) {
     const frankieSize = size || new Vector(3.5, 3.5);
     const frankieXSpeed = xSpeed || 12;
-    const frankieJumpSpeed = jumpSpeed || 40;
+    const frankieJumpSpeed = jumpSpeed || 10;
     super(pos, ch, speed, frankieSize, frankieXSpeed, frankieJumpSpeed);
 }
 }
